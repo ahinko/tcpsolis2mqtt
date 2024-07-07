87895a226811894f632be73b35ad714068cac48c
Peter Ahinko <peter@ahinko.se>
2024-07-07 20:18:16 +0200

    feat: prepare for version 1.1.0

--- app/app.py
+++ app/app.py
@@ -23,7 +23,7 @@ from pymodbus.transaction import ModbusSocketFramer
 from pymodbus.constants import Endian
 from pymodbus.payload import BinaryPayloadDecoder
 
-VERSION = "1.0.3"
+VERSION = "1.1.0"
 
 
 class App:
